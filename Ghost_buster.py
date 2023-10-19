e6c701875313031ec0b94a44ef40778c1c089878
Darren Clavette <clavettd@oregonstate.edu>
2023-10-19 13:35:34 -0700

    added pseudo code

--- Ghost_buster.py
+++ Ghost_buster.py
@@ -1,1 +1,25 @@
 #This is a python script for testing ghost vs ingroup introgression
+
+
+
+# inport needed modules
+
+import os
+import glob
+#import numpy as np 
+#import pandas as pd
+#from Bio import SeqIO
+#from Bio import AlignIO
+
+
+
+#get a list of seq files to analyze 
+	#store as python list object
+
+#print the number of files in this list.
+
+#read in the first file in the list and store as a python alignment object.
+
+#print the number of sites in the alignment. 
+
+
