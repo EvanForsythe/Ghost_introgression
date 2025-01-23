8cd0f8cd3b5e7dc940af4d8b95bbed7a9657795a
EvanForsythe <esfors@rams.colostate.edu>
2025-01-23 11:36:15 -0800

    added script to make param plots, which is hardcoded currently

---
+++ Param_scan_plots.py
@@ -0,0 +1,59 @@
+#Load packages
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Import data into a dataframe
+sim_df = pd.read_table('/scratch/pappab/Ghost_project/Ghost_introgression/Sim_stats_log.tsv')
+quant_df = pd.read_table('/scratch/pappab/Ghost_project/Ghost_introgression/Quant_results_log.tsv')
+
+
+scaling_quant_df = quant_df[quant_df["Job_name"].str.contains("scale_")]
+
+
+sf_list=list()
+rep_list=list()
+Int_type_list=list()
+
+for row in scaling_quant_df.index:
+    temp_jobname = scaling_quant_df.loc[row]["Job_name"]
+    temp_sf=temp_jobname.split("_")[4]
+    temp_rep=temp_jobname.split("_")[-1]
+    temp_type=temp_jobname.split("_")[1]
+    
+    sf_list.append(float(temp_sf))
+    rep_list.append(int(temp_rep))
+    Int_type_list.append(str(temp_type))
+    #scaling_quant_df.loc[row]["Scaling_factor"] = temp_sf
+    #scaling_quant_df.loc[row]["Replicate"] = temp_rep
+
+scaling_quant_df["Scaling_factor"] = sf_list
+scaling_quant_df["Replicate"] = rep_list
+scaling_quant_df["Int_type"] = Int_type_list
+
+#Create plot of results
+sns.scatterplot(
+    data=scaling_quant_df, 
+    x="Scaling_factor", 
+    y="rep_pop_mean", 
+    hue="Int_type",  # Categorical column for color mapping
+    palette="viridis",  # Use the same colormap as before
+    edgecolor="black"  # Optional for better visibility
+)
+
+# Optionally highlight specific subset (if needed)
+# subset_df = scaling_quant_df[scaling_quant_df["Job_name"].str.contains("true")]
+# sns.scatterplot(data=subset_df, x="Scaling_factor", y="rep_pop_mean", color="red", label="True Jobs")
+
+# Add legend, labels, and title
+plt.legend(title='Introgression type')
+plt.xlabel('Scaling Factor')
+plt.ylabel('Delta (average from bootstrap resampling)')
+plt.title('Scaling factor parameter scan plot')
+
+# Save the figure
+plt.savefig('Scaling_factor_fig.pdf', format="pdf")
+
+plt.show()
